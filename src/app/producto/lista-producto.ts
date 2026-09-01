520404d14e9338703bdf7a616f531e89ce5cf589
rv-jgomez <josegomezalberto11@gmail.com>
2026-09-01 13:51:33 -0600

    FEAT: Se creó la interfaz de usuario para la lista de productos y confirmación de eliminación de productos
    Se sustituyó la plantilla de marcador de posición por una vista completa de la lista de productos (tabla) y un mensaje condicional en caso de que la lista esté vacía. Implementa el componente «ListaProducto» para cargar los productos a través de «ProductoService» al iniciarse, controlar el estado de lista vacía y forzar la detección de cambios. Además se añade un flujo de eliminación con una ventana de confirmación de SweetAlert2 que elimine un elemento y recargue la lista. Añade también «RouterModule» y las importaciones para el modelo y el servicio «Producto», así como un manejo básico de errores.

--- src/app/producto/lista-producto.ts
+++ src/app/producto/lista-producto.ts
@@ -1,9 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Producto } from '../models/producto';
+import { ProductoService } from '../services/producto.service';
+import { RouterModule } from '@angular/router';
+
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-lista-producto',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './lista-producto.html',
   styleUrl: './lista-producto.css',
 })
-export class ListaProducto {}
+export class ListaProducto implements OnInit{
+  productos: Producto[] = []
+
+  listaVacia: string | undefined = undefined;
+
+  constructor(
+    private productoService: ProductoService,
+    private cdr: ChangeDetectorRef
+  ){  }
+
+  ngOnInit(): void {
+    this.cargarProductos();
+  }
+
+  cargarProductos(): void {
+    this.productoService.lista().subscribe(
+      data => {
+        this.productos = data;
+        if (this.productos.length === 0) {
+            this.listaVacia = 'No hay productos registrados';
+        } else {
+            this.listaVacia = undefined;
+        }
+        this.cdr.detectChanges();
+      },
+      err => {
+        this.listaVacia = err.error.message;
+        this.cdr.detectChanges();
+      }
+    );
+  }
+
+  borrar(id: number): void {
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: 'No hay vuelta atrás',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sip',
+      cancelButtonText: 'Nops'
+    }).then((result) => {
+      if (result.value) {
+        this.productoService.delete(id).subscribe(res => {
+          this.cargarProductos();
+        });
+        Swal.fire(
+          'OK',
+          'Producto eliminado',
+          'success'
+        );
+      // For more information about handling dismissals please visit
+      // https://sweetalert2.github.io/#handling-dismissals
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire(
+          'Cancelado',
+          'Producto a salvo',
+          'error'
+        );
+      }
+    });
+  }
+}

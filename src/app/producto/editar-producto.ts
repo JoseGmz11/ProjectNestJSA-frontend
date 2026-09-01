67bd51053ce62ada506e35c722ec3088a6799823
rv-jgomez <josegomezalberto11@gmail.com>
2026-09-01 14:01:25 -0600

    FEAT: Se creó el formulario de edición de productos y el flujo de actualización
    Este cambio añade una pantalla de edición de productos que carga el producto seleccionado por su ID, muestra un formulario para editar su nombre y precio, y valida los campos obligatorios antes de enviarlo. Además, conecta el formulario con el punto final de actualización del servicio de productos, muestra mensajes emergentes de éxito o error y vuelve a la vista de inicio tras una actualización correcta o cuando falla la carga.

--- src/app/producto/editar-producto.ts
+++ src/app/producto/editar-producto.ts
@@ -1,9 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { ProductoService } from '../services/producto.service';
+import { ToastrService } from 'ngx-toastr';
+import { Producto } from '../models/producto';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-editar-producto',
-  imports: [],
+  imports: [ RouterModule, FormsModule ],
   templateUrl: './editar-producto.html',
   styleUrl: './editar-producto.css',
 })
-export class EditarProducto {}
+export class EditarProducto implements OnInit{
+
+  producto?: Producto;
+
+  constructor(
+    private productoService: ProductoService,
+    private activatedRoute: ActivatedRoute,
+    private toastr: ToastrService,
+    private router: Router,
+    private cdr: ChangeDetectorRef
+  ) {}
+  
+  ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.params['id'];
+    this.productoService.detail(id).subscribe(
+      data => {
+        this.producto = data;
+        this.cdr.detectChanges();
+      },
+      err => {
+        this.toastr.error(err.error.message, 'Fail', {
+          timeOut: 3000,
+          positionClass: 'toast-top-center',
+        });
+        this.volver();
+      }
+    );
+  }
+
+  onUpdate(): void {
+    const id = this.activatedRoute.snapshot.params['id'];
+    this.productoService.update(id, this.producto!).subscribe(
+      data => {
+        this.toastr.success(data.message, 'OK', {
+          timeOut: 3000,
+          positionClass: 'toast-top-center',
+        });
+        setTimeout(() => {
+          this.router.navigate(['/']);
+        }, 50);
+      },
+      err => {
+        this.toastr.error(err.error.message, 'Fail', {
+          timeOut: 3000,
+          positionClass: 'toast-top-center',
+        });
+      }
+    );
+  }
+
+  volver(): void {
+    this.router.navigate(['/']);
+  }
+}

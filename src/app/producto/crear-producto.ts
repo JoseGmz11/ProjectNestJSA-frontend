245d304ff2261457c4e366a8bcaa8a945c3565f1
rv-jgomez <josegomezalberto11@gmail.com>
2026-09-01 13:56:15 -0600

    FEAT: Se creó el formulario y la lógica de creación de productos
    Se sustituye el marcador de posición por un formulario de Bootstrap para crear productos (nombre, precio) y añade la lógica del componente para gestionar la creación. Implementa el enlace bidireccional con ngModel (se ha añadido FormsModule al componente), inyecta ProductoService, ToastrService y Router, y llama a productoService.save(...) con notificaciones Toastr de éxito o error y navegación de vuelta a la página de inicio. También añade un helper volver() y un esqueleto básico de ngOnInit.

--- src/app/producto/crear-producto.ts
+++ src/app/producto/crear-producto.ts
@@ -1,9 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductoService } from '../services/producto.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { Producto } from '../models/producto';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-crear-producto',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './crear-producto.html',
   styleUrl: './crear-producto.css',
 })
-export class CrearProducto {}
+export class CrearProducto implements OnInit {
+  nombre = '';
+  precio: number = null!;
+
+  constructor(
+    private productoService: ProductoService,
+    private toastr: ToastrService,
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {}
+
+  onCreate(): void {
+    const producto = new Producto(this.nombre, this.precio);
+    this.productoService.save(producto).subscribe(
+      (data) => {
+        this.router.navigate(['/']).then(() => {
+          this.toastr.success(data.message, 'OK', {
+            timeOut: 3000,
+            positionClass: 'toast-top-center',
+          });
+        });
+      },
+      (err) => {
+        this.toastr.error(err.error.message, 'Fail', {
+          timeOut: 3000,
+          positionClass: 'toast-top-center',
+        });
+      },
+    );
+  }
+
+  volver(): void {
+    this.router.navigate(['/']);
+  }
+}

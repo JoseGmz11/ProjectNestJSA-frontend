5af9df4acc1b85c81c43793e375ca488b0b23833
rv-jgomez <josegomezalberto11@gmail.com>
2026-09-01 13:59:55 -0600

    FEAT: Se creó la vista de detalles del producto y la lógica de los componentes
    Se ha sustituido la plantilla de marcador de posición por una tarjeta de Bootstrap que muestre los detalles del producto y un botón de volver. Implementar el componente `DetalleProducto`: se ha añadido `OnInit`, la lectura de los parámetros de ruta, la llamada a `ProductoService` para obtener el detalle (id), la asignación del resultado correcto a `producto` y la gestión de errores con `ngx-toastr` y la navegación hacia atrás. Se han inyectado ActivatedRoute, Router, ToastrService, ProductoService y ChangeDetectorRef (que llama a detectChanges). También se ha añadido RouterModule a las importaciones del componente y se han incluido pequeños registros de consola para la depuración.

--- src/app/producto/detalle-producto.ts
+++ src/app/producto/detalle-producto.ts
@@ -1,9 +1,48 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Producto } from '../models/producto';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { ProductoService } from '../services/producto.service';
 
 @Component({
   selector: 'app-detalle-producto',
-  imports: [],
+  imports: [ RouterModule ],
   templateUrl: './detalle-producto.html',
   styleUrl: './detalle-producto.css',
 })
-export class DetalleProducto {}
+export class DetalleProducto implements OnInit{
+
+  producto?: Producto;
+
+  constructor(
+    private productoService: ProductoService,
+    private activatedRoute: ActivatedRoute,
+    private toastr: ToastrService,
+    private router: Router,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.params['id'];
+    console.log('1. ID capturado de la URL:', id);
+    this.productoService.detail(id).subscribe(
+      data => {
+        console.log('2. Datos recibidos con éxito:', data);
+        this.producto = data;
+        this.cdr.detectChanges();
+      },
+      err => {
+        this.toastr.error(err.error.message, 'Fail', {
+          timeOut: 3000,
+          positionClass: 'toast-top-center',
+        });
+        this.volver();
+        console.log('Error completo',err);
+      }
+    );
+  }
+
+  volver(): void {
+    this.router.navigate(['/']);
+  }
+}
